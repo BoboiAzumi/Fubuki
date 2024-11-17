04c7db8381547d084ba2052e87853522e5d6a21a
BoboiAzumi <untukherokudeploy@gmail.com>
2024-11-17 09:10:10 +0700

    feat: storage system

--- src/routes/file-system.ts
+++ src/routes/file-system.ts
@@ -1,6 +1,6 @@
 import { Hono, type Context } from "hono";
 import { FileRequests } from "../global/file-requests";
-import { copy, list, mkdir, remove, touch } from "../service/file-system";
+import { copy, list, mkdir, remove, touch, writeUpload } from "../service/file-system";
 import _path from "path"
 import dotenv from "dotenv"
 
@@ -57,6 +57,32 @@ fileSystemRouter.post("/", async (c: Context) => {
                     status: "OK"
                 })
         }
+    }
+    catch(err: unknown){
+        c.status(400)
+        return c.json({
+            status: "ERROR",
+            message: (err as Error).message
+        })
+    }
+})
+
+fileSystemRouter.post("/upload", async (c: Context) => {
+    try {
+        const { files, target } = await c.req.parseBody({all: true})
+
+        if(Array.isArray(files)){
+            for(let i = 0; i < files.length; i++){
+                writeUpload(_path.join(DEFAULT_DIR, target as string, (files[i] as File).name), await (files[i] as File).arrayBuffer())
+            }
+        }
+        else{
+            writeUpload(_path.join(DEFAULT_DIR, target as string, (files as File).name), await (files as File).arrayBuffer())
+        }
+
+        return c.json({
+            status: "OK"
+        })
     }
     catch(err: unknown){
         c.status(400)
