f18aac78e9d40e00514f93e121ade6258afc4006
BoboiAzumi <untukherokudeploy@gmail.com>
2024-11-21 13:40:48 +0700

    fix: memory leak

--- src/routes/manager.ts
+++ src/routes/manager.ts
@@ -1,6 +1,25 @@
 import { Hono } from "hono";
-import { fileDownload } from "../handler/file-download";
+import { serveStatic } from "hono/serve-static";
+import fs from 'fs'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const managerRouter = new Hono()
 
-managerRouter.get('*', fileDownload)
+managerRouter.get('*', serveStatic({
+    root: './',
+    getContent: async (path) => {
+        try{
+            path = path.replace('file', `${process.env.ROOT_DIR}`)
+            return fs.readFileSync(path) as any
+        }
+        catch{
+            return new Response(JSON.stringify({status: "File Not Found"}), {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+        }
+    }
+}))
